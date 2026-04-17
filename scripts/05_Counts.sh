ed5b089a88ee52d893747514ffc870183c6f5165
Souvik Bhaduri <souvikbhaduri2001@gmail.com>
2026-04-17 15:51:28 +0530

    Add script for counting features using featureCounts

---
+++ scripts/05_Counts.sh
@@ -0,0 +1,25 @@
+set -e
+set -o pipefail
+
+# -------------------------------
+# CONFIGURATION
+# -------------------------------
+
+BAM_DIR="results/alignment"
+OUT_DIR="results/counts"
+GTF="reference/Caenorhabditis_elegans.WBcel235.110.gtf"
+THREADS=8
+
+mkdir -p "$OUT_DIR"
+
+echo "Running featureCounts..."
+
+featureCounts \
+    -T $THREADS \
+    -a "$GTF" \
+    -o "$OUT_DIR/gene_counts.txt" \
+    -t exon \
+    -g gene_id \
+    $BAM_DIR/*.bam
+
+echo "Counting completed!"
