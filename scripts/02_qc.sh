c55269e748aaa70b930b2a71cf6e4a10c5f2c32d
Souvik Bhaduri <souvikbhaduri2001@gmail.com>
2026-04-17 09:05:15 +0530

    Add QC script for FastQC and MultiQC processing

--- scripts/02_qc.sh
+++ scripts/02_qc.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+set -e
+
+# -------------------------------
+# CONFIGURATION
+# -------------------------------
+
+FASTQ_DIR="data"
+OUT_DIR="results/qc"
+THREADS=8
+
+mkdir -p $OUT_DIR
+
+# Run FastQC on all FASTQ files
+fastqc ${FASTQ_DIR}/*.fastq.gz -o $OUT_DIR -t $THREADS
+
+# Run MultiQC to summarize reports
+multiqc $OUT_DIR -o $OUT_DIR
+
+echo "QC completed!"
